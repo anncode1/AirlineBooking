a97f2caa98de19a4026f0e1671393e4ccf91dcdd
SALGA041 <anahi.x.salgado.-nd@disney.com>
2022-04-16 19:29:26 -0500

    clase-21 Changing name to TicketDataDI from TicketDataUI

--- src/domain/usecases/ticket/di/TicketDataDI.kt
+++ src/domain/usecases/ticket/di/TicketDataDI.kt
@@ -3,7 +3,7 @@ package domain.usecases.ticket.di
 import data.ticket.TicketSingleton
 import domain.datasource.ticket.TicketDataSource
 
-class TicketDataUI {
+class TicketDataDI {
     fun providesTicket(): TicketDataSource {
         return TicketSingleton()
     }
