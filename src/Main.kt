6c63e3e7014f65b83cc96c3425d0a4844e1b21fb
SALGA041 <anahi.x.salgado.-nd@disney.com>
2022-04-16 19:22:05 -0500

    clase-18 Baggage Packages without prices

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,34 @@
 
+import data.airbooking.AirBookingLocalSource
+import data.aircraft.AirCraftLocalSource
+import data.airport.AirportLocalSource
 import data.baggage.BaggageRegularLocalSource
 import data.baggage.BaggageVClubLocalSource
+import data.flight.FlightLocalSource
 import data.ticket.TicketSingleton
 import domain.datasource.baggage.BaggagePackageDataSource
 import domain.usecases.baggage.GetBaggagePackages
+import domain.usecases.flight.GetFlights
 import domain.usecases.ticket.AssignFlightToTicket
 import presentation.baggage.BaggagePackageConsole
 import presentation.baggage.types.BaggageTypesConsole
+import presentation.flight.formats.FlightConsoleFormat
+import java.time.Month
 
 fun main() {
+
+    val airportDataSource = AirportLocalSource()
+    val airportBookingLocalSource = AirBookingLocalSource(airportDataSource)
+    val airCraftLocalSource = AirCraftLocalSource()
+
+    // Empezar aquí y luego ir para atrás
+    val flightLocal = FlightLocalSource(airCraftLocalSource, airportBookingLocalSource)
+    val getFlights = GetFlights(flightLocal).invoke(Month.JANUARY)
+    getFlights.forEach { (t, u) ->
+        print("$t. ")
+        println(FlightConsoleFormat().format(u))
+    }
+
 
     // 7. Crear
     println()
@@ -18,23 +38,6 @@ fun main() {
 
     val regularLocalSource = BaggageRegularLocalSource()
     val getRegularPackages = GetBaggagePackages(regularLocalSource).invoke()
-
-    /*println("*** VClub Baggage's ***")
-    getVClubPackages.forEach { (t, u) ->
-        print("$t. ")
-        println(BaggagePackageConsole(
-            BaggageTypesConsole()
-        ).format(u))
-    }
-
-    println()
-    println("*** Regular Baggage's ***")
-    getRegularPackages.forEach { (t, u) ->
-        print("$t. ")
-        println(BaggagePackageConsole(
-            BaggageTypesConsole()
-        ).format(u))
-    }*/
 
     // 8 crear
     println("*** VClub Baggage's ***")
